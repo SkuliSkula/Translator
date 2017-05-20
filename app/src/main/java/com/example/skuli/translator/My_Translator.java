82a79e9af951915f7533240deb1e6f387ccf0dcf
Skúli <skuliskula84@gmail.com>
2017-05-20 15:38:37 +0200

    Initial commit

---
+++ app/src/main/java/com/example/skuli/translator/My_Translator.java
@@ -0,0 +1,13 @@
+package com.example.skuli.translator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class My_Translator extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my__translator);
+    }
+}
